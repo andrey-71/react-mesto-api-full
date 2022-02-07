b395c4e74c98e9fd11552286a48994f73839db86
andrey-71 <and_go1991@mail.ru>
2022-02-08 00:46:29 +0300

    refactor: изменить порт локального сервера на 3001

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,5 +1,5 @@
 const {
-  PORT = 3000,
+  PORT = 3001,
   DB_ADDRESS = 'mongodb://localhost:27017/mestodb',
   JWT_SECRET = 'e4d525267573958a5c953cade4b1fc177e26fa56c1c5e1700b6f799abd8668b7',
 } = process.env;
