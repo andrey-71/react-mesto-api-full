e6b4af653dcffdfc1cfc5f97cc843d828edc0084
andrey-71 <and_go1991@mail.ru>
2022-02-12 17:17:02 +0300

    refactor: исправить ошибки линтера в директории frontend

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -2,8 +2,7 @@ import React from 'react';
 import {Navigate} from 'react-router-dom';
 
 const ProtectedRoute = (props) => {
-  {/* Доступ на главную страницу только для авторизованных пользователей */
-  }
+  /* Доступ на главную страницу только для авторизованных пользователей */
   return props.isLogin ? props.children : <Navigate to='/sign-in'/>}
 
 export default ProtectedRoute;
